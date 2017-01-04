a8a1cf7eab6a93fac1b9a68c8b4ff72a093b7701
Steven Tucker <stucker@PH.local>
2017-01-04 11:57:05 -0700

    added index.html and test.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<html>
+	<body>
+		Welcome to the test page. To find updates, please see below.
+		
+		<h1>Update 1.0.1 Summary</h1>
+		We have been working on adding normal functions to posts, pages, and site settings. Once completed we will be continuing our efforts towards user management.
+		<p>
+		
+		<i>Posts</i><br>
+		Added the ability to add a post. Will be working next on adding a proper editing ui and defining title, name, and urls.
+		<p>
+		
+		<i>Pages</i><br>
+		Added the ability to add a page. Will be working next on adding a proper editing ui and defining title, name, and urls.
+		<p>
+		
+		<i>Site Settings</i><br>
+		We created the page but still working on creating a database relation for the site.
+	</body>
+</html>
